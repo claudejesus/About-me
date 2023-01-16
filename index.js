30d2b5885e12320260d450dbbc5041b9a099f610
unknown <niyigabaclaujesus@gmail.com>
2023-01-16 17:00:56 +0200

    my skills

---
+++ index.js
@@ -0,0 +1,10 @@
+const allPage = document.getElementById("#all");
+const brandinggPage = document.getElementById("#branding");
+const fashionPage = document.getElementById("#fashion");
+const productPage = document.getElementById("#product");
+
+allPage.addEventListener("click", handleAllPage);
+
+function handleAllPage() {
+  alert("wellcome to all pages");
+}
